e3908cb09fc7c9643c89d42eadbbb132e3973fbf
emberfarkas <hubing91@163.com>
2016-11-11 18:17:37 +0800

    fix

--- Assets/tool/util.lua
+++
@@ -1,4 +0,0 @@
-package.path = "./../3rd/sproto-Csharp/tools/?.lua;" .. package.path
-dofile("mk_sproto.lua")
-os.execute("cd ./../3rd/sproto-Csharp/tools && ./lua sprotodump.lua -cs ./../../../tool/c2s.sproto -d ./../../../tool/ -p c2s")
-os.execute("cd ./../3rd/sproto-Csharp/tools && ./lua sprotodump.lua -cs ./../../../tool/s2c.sproto -d ./../../../tool/ -p s2c")
